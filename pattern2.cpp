5bc199f92897cac3aeed1a0eb9c149768bb92091
Sonali <110111616+sonalikhedkar@users.noreply.github.com>
2023-09-15 22:51:55 +0530

    Update pattern2.cpp

--- pattern2.cpp
+++ pattern2.cpp
@@ -10,14 +10,32 @@ int main(){
         }
         cout<<endl;
     }
-    return 0;
+   
 
-}
+
 /*
 1 
 1 2 
 1 2 3 
 1 2 3 4 
 */
+
+    for(int i=1;i<=row;i++){
+        for(int j=1;j<=i;j++){
+            cout<<i<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+
+}
+/*
+1 
+2 2 
+3 3 3 
+4 4 4 4
+*/
+
+
 
 
