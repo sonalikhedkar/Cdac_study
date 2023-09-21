30082286c66f4137e03ee20d146e2ad8a7cac7fa
Sonaliii <110111616+sonalikhedkar@users.noreply.github.com>
2023-09-21 01:04:44 -0700

    Add files via upload

---
+++ 21Sept23_Polymorphism/staticpoly_case2.cpp
@@ -0,0 +1,80 @@
+#include<iostream>
+using namespace std;
+class employee{
+	int id;
+	public:
+	employee(){
+		id =0;
+		cout<<"in the default of emp"<<endl;
+	}
+	employee(int i){
+		id=i;
+	}
+	int findsalary(){
+		return 0;
+	}
+	void display(){
+		cout<<"salary is "<<findsalary()<<endl;
+	}
+	
+};
+class wageemployee: public employee{
+	int hrs;
+	int rate;
+	public:
+		wageemployee(){
+			cout<<"int default of wage"<<endl;
+		}
+		wageemployee(int,int,int);
+    int findsalary();
+ 	void display(){
+		cout<<"salary is "<<findsalary()<<endl;
+	}
+	
+	
+};
+wageemployee::wageemployee(int i,int h,int r):employee(i){
+	hrs=h;
+	rate=r;
+}
+int wageemployee::findsalary(){
+	return hrs*rate;
+}
+class salesmanger:public wageemployee
+{
+	int sales,comm;
+	public:
+		salesmanger(){
+			cout<<"in default of sale"<<endl;
+		}
+		salesmanger(int,int,int,int,int);
+	int findsalary();
+		void display(){
+		cout<<"salary is "<<findsalary()<<endl;
+	}
+	
+};
+salesmanger::salesmanger(int i,int r,int h,int s,int c):wageemployee( i,h,r){
+	sales=s;
+	comm=c;
+	
+}
+int salesmanger::findsalary(){
+	return (wageemployee::findsalary())+sales*comm;
+	}
+int main()
+{
+ employee *ptr=new employee();
+ ptr->findsalary();
+ ptr->display();
+ ptr=new wageemployee();
+ ptr->findsalary();
+ ptr->display(); 
+ ptr=new salesmanger();
+ ptr->findsalary();
+ ptr->display(); 
+	
+ if(ptr)
+  delete ptr;
+  ptr=NULL;	
+}
